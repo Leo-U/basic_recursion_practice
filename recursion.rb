c18017d59c4ead156e28196fa2f4e17cfcb51fa1
Joel Howard <joelhoward2@gmail.com>
2023-01-30 13:43:59 -0500

    clear old problems

---
+++ recursion.rb
@@ -0,0 +1,15 @@
+#first, write a non-recursive method that sums the range of 1 thru n from given input `n`.
+
+def sum_range(n)
+  total = n
+  i = n
+  while i > 0 do
+    i -= 1
+    total += i
+    puts total
+  end
+  total
+end
+
+# now, write a recursive version.
+
