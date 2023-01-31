3bf9bb4b6a2d456be8ebf07addaf8ef90a251aaa
Joel Howard <joelhoward2@gmail.com>
2023-01-31 15:30:21 -0500

    complete my version fibonacci method

--- recursion.rb
+++ recursion.rb
@@ -76,3 +76,12 @@ def bottles(n)
     bottles(n - 1)
   end
 end
+
+# Define a recursive function that takes an argument n and returns the fibonacci value of that position. The fibonacci sequence is 0, 1, 1, 2, 3, 5, 8, 13, 21... So fib(5) should return 5 and fib(6) should return 8.
+
+# my solution:
+def return_fib_value(n, a = 0, b = 1)
+  return 1 if n == 1
+  return 0 if n == 0
+  return_fib_value(n - 1, b, a + b)
+end
